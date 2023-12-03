7304533a9952cae345ddd9de70de3c9f31ca0930
ChinmayGawad <chinmaygawad86@gmail.com>
2023-12-03 13:06:29 +0530

    Re-create home_page

--- app/src/main/java/com/pgshare/studentpgapp/MainActivity.java
+++ app/src/main/java/com/pgshare/studentpgapp/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
     public void Rent(View view) {
-        Intent intent=new Intent(MainActivity.this, HomePageActivity.class);
+        Intent intent=new Intent(MainActivity.this, home_page.class);
         startActivity(intent);
     }
     public void Pg(View view) {
